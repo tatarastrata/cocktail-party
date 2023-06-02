072e2ea92f6cd002c7b0618e1363b80cc94ab7d3
tamara <22053528+tatarastrata@users.noreply.github.com>
2023-06-02 19:18:29 +0300

    deploy branch

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'production',
@@ -6,6 +8,18 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.name': JSON.stringify('Tomik prod'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../build'),
+          to: path.resolve(__dirname, '../'),
+        },
+        {
+          from: path.resolve(__dirname, '../public'),
+          to: path.resolve(__dirname, '../'),
+        },
+      ],
     }),
   ],
   output: {
